fef51b06cb69ca013f1bca3f1b71186af4164bd6
quandt <hosyquan06012004@gamil.com>
2023-08-10 09:40:15 +0700

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutsComponent } from './components/abouts/abouts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    BannerComponent,
+    AboutsComponent
   ],
   imports: [
     BrowserModule,
