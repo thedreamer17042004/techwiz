b27ed19cbc8bcdef4409c041ec6c5e145ab2defc
NamNguyen <namlc247@gmail.com>
2023-08-11 21:38:45 +0700

    fix

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -8,78 +8,42 @@ declare var $: any;
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
-export class DetailsComponent implements OnInit{
-  dataDetail:any;
+export class DetailsComponent implements OnInit {
+  dataDetail: any = {};
   products: any = [];
 
 
 
-  constructor(private product: MyAppService, private route: ActivatedRoute){}
-  
-    ngOnInit(): void {
-      // carousel blogHome
-      $('.detail-carow').owlCarousel({
-        loop:true,
-        margin:10,
-        dots: false,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:2
-            },
-            1000:{
-                items:3
-            }
-        }
-      })
-      $('.detail-right-special').owlCarousel({
-        loop:true,
-        margin:10,
-        dots: false,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:2
-            },
-            1000:{
-                items:1
-            }
-        }
-      })
-        // owlcarowsel thay đổi btn slidering
-      $(document).ready(function(){
-       
-          // parralax
-          $('.parallax-window').parallax({imageSrc: 'http://lovesome.biz/tf-template/garden/img/page-title-bg.jpg'});
-    
-        })
-    
-        this.product.getData().subscribe((res: any) => {
-          this.products = res.products
-          this.route.paramMap.subscribe(params => {
-            let id =  params.get('id');
-            this.dataDetail = this.getProductById(id);
-          });
-        })
+  constructor(
+    private product: MyAppService,
+    private route: ActivatedRoute,
+    private app: MyAppService
+  ) { }
 
- 
-      
+  ngOnInit(): void {
+    // owlcarowsel thay đổi btn slidering
+    $(document).ready(function () {
 
+      // parralax
+      $('.parallax-window').parallax({ imageSrc: 'http://lovesome.biz/tf-template/garden/img/page-title-bg.jpg' });
 
-      
+    })
+
+    this.product.getData().subscribe((res: any) => {
+      this.products = res.products
+      this.route.paramMap.subscribe(params => {
+        let id: any = params.get('id');
+        this.dataDetail = this.product.getProductById(id, this.products);
+      });
+    })
+
+  }
+
+  addToCart(id: any) {
+    let prd = this.app.getProductById(id, this.products);
+    let check = this.app.savePrd(prd);
+    if (check) {
+      alert("Success");
     }
-    
-      getProductById(id:any) {
-          let info:any = this.products.find((item: any) => {
-            return item.id == id
-        });
-        console.log(info)
-      
-         return info;
-         
-      }
+  }
 }
